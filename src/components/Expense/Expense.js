62cbb6d9f92566d39f33ecfe6d3c38f2383efda8
pranav2805 <pranavpradeep095@gmail.com>
2023-11-24 20:28:31 +0530

    Verify User Email

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,7 +1,33 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import AuthContext from "../../store/auth-context";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Expense = (props) => {
   const isProfileUpdated = localStorage.getItem("isProfileUpdated");
+  const authCtx = useContext(AuthContext);
+
+  const verifyEmailHandler = async () => {
+    const token = authCtx.token;
+    const resp = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+        }),
+      }
+    );
+    if (resp.ok) {
+      alert("Verification email sent successfully");
+    } else {
+      console.log("something went wrong");
+    }
+  };
+
   return (
     <div>
       Welcome to Expense Tracker
@@ -16,6 +42,9 @@ const Expense = (props) => {
             <Link to="/profile">Update your profile</Link>
           </p>
         )}
+        <Button type="submit" onClick={verifyEmailHandler}>
+          Verify Email
+        </Button>
       </div>
     </div>
   );
